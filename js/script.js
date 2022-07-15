d06fa4648837285ee3317b0634e1e4326687b498
elmarkazumov <elmarkazumov20@gmail.com>
2022-07-15 23:29:19 +0500

    working version

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
+let inputText = document.querySelector('#main__input'),
+    buttonCheck = document.querySelector('#main__btn');
 
-function check(){
+buttonCheck.onclick = function (){
+    let array = []; 
+    array.push(inputText.value.toLowerCase());
+    
+    let arrayString = array.toString(),
+        arraySplit = arrayString.split(''),
+        count = 0;
 
+    for(let i = 0; i < arraySplit.length; i++){
+        if(arraySplit[i] == arraySplit[arraySplit.length - 1 - i]){
+            count++;
+        }
+        if(count == arraySplit.length){
+            document.querySelector('.main__text').innerHTML = "<p>Текст является палиндромом</p>";
+        } else{
+            document.querySelector('.main__text').innerHTML = "<p>Текст не является палиндромом</p>";
+        }
+    }
 }
